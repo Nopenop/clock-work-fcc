3f14b33f5455902d16e239ac12c846042f9bf33d
Noah <111076387+Nopenop@users.noreply.github.com>
2024-02-12 11:09:51 -0500

    budget-app.py works

--- budget-app.py
+++ budget-app.py
@@ -1,69 +1,58 @@
 class Category:
-    def __init__(self, cat_name:str):
-        self.cat_name = cat_name
-        self.descriptions = {"":0}
-    def __str__(self):
+    def __init__(self, title:str):
+        self.ledger = []
+        self.title = title
+    def __str__(self) -> str:
         retString = ""
-        retString += f'{self.cat_name:*^30}\n'
-        for key in self.descriptions.keys():
-            retString += f'{key[:23]:<23s}{self.descriptions[key]:7.2f}\n'
-        retString += f'Total: {sum(self.descriptions.values()):7.2f}'
+        retString += self.title.center(30, "*") + "\n"
+        for item in self.ledger:
+            retString += f'{item["description"][:23]:23}' + f'{item["amount"]:>7.2f}' + "\n"
+        retString += f'Total: {self.get_balance():.2f}'
         return retString
-    def deposit(self, amount:float, description:str):
-        self.descriptions[description] = amount
-    def withdraw(self, amount:float, description:str=""):
-        if not self.check_funds(-amount):
-            return False
-        if description == "":
-            self.descriptions[""] -= amount
-        self.descriptions[description] = -amount
+
+    def deposit(self, amount:float ,description: str = ""):
+        self.ledger.append({"amount": amount, "description": description})
+    def withdraw(self, amount:float, description: str = ""):
+        if not self.check_funds(amount):
+            return False  
+        self.ledger.append({"amount": -amount, "description": description})
         return True
-    def get_balance(self):
-        return sum(self.descriptions.values())
-    def transfer(self, amount:float, budget_name):
-        if self.withdraw(amount,f'Transfer to {budget_name.cat_name}'):
-            budget_name.deposit(amount, f'Transfer from {self.cat_name}')
+    def get_balance(self) -> float:
+        return sum(item["amount"] for item in self.ledger)
+    def transfer(self, amount:float, category):
+        if self.withdraw(amount, f'Transfer to {category.title}'):
+            category.deposit(amount, f'Transfer from {self.title}')
             return True
         return False
-    def check_funds(self, amount:float):
-        return 0 < sum(self.descriptions.values()) + amount
+    def getWithdrawal(self) -> float:
+        return sum(item['amount'] for item in self.ledger if item['amount'] < 0)
+    def check_funds(self, amount:float) -> bool:
+        return self.get_balance() >= amount
 
 
 def create_spend_chart(categories):
-    retString = ""
-    retString += "Percentage spent by category\n"
-    total = sum(category.get_balance() for category in categories)
-    percentages = list(int(((category.get_balance()/total)*10))*10 for category in categories)
-    print(percentages)
-    for i in range(100, -1 , -10):
-        retString += f'{i:>3}|'
-        for percentage in percentages:
-            if percentage >= i:
-                retString += ' O '
+    retString = "Percentage spent by category\n"
+    # values = list(item.get_balance() for item in categories)
+    # total = sum(values)
+    for i in range(100, -1, -10):
+        retString += f'{i:3}|'
+        for item in categories:
+            if (item.getWithdrawal()/sum(list(cat.getWithdrawal() for cat in categories)))*100 > i:
+                retString += " o "
             else:
-                retString += '   '
-        retString += ' \n'
-    retString += '    '
-    for i in range(len(percentages)):
-        retString+='---'
-    retString+='-\n'
-    for i in range(max(len(category.cat_name) for category in categories)):
-        retString+="    "
-        for j in range(len(categories)):
+                retString += "   "
+        retString += " \n"
+    retString += "".rjust(4) + "".ljust(len(categories)*3, "-") + "-\n"
+
+    for i in range(max(len(item.title) for item in categories)):
+        retString += f'    '
+        for item in categories:
             try:
-                retString += f' {categories[j].cat_name[i]} '
+                retString += f' {item.title[i]} '
             except IndexError:
                 retString += '   '
+        if i == max(len(item.title) for item in categories)-1:
+            retString += " "
+            continue
         retString += ' \n'
     return retString
-
-
-
-food = Category("Food")
-food.deposit(1000, "Initial Deposit")
-food.withdraw(200, "Chicken")
-orange_juice = Category("Orange Juice")
-food.transfer(200,orange_juice)
-print(food)
-
-print(create_spend_chart([food, orange_juice]))
